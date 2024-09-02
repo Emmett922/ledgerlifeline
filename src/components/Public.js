5dda3d8ccf6facda8a9979e9fa85412208f836dc
Emmett922 <emmett.peters922@gmail.com>
2024-09-01 22:07:14 -0400

    Started fronted foundation

---
+++ src/components/Public.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+// Main html content page goes here
+// Do not have
+const Public = () => {
+  const content = (
+    <section className='public'></section>
+  )
+  return content
+}
+
+export default Public
